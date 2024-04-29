ff401a7d3e535efbe1c42f258beadec4976d459a
romanie delporte <romaniedelporte230@gmail.com>
2024-04-29 21:12:12 +0200

    setup one pager with next.js

---
+++ my-app/src/app/card.tsx
@@ -0,0 +1,10 @@
+
+export default function Card() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="flex bg-red-500">
+                <h1 className="font-bold text-3xl leading-tight">first one pager in react</h1>
+            </div>
+        </main>
+    );
+}
---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex bg-red-500">
+        <h1 className="font-bold text-3xl leading-tight">first one pager in react</h1>
+      </div>
+    </main>
+  );
+}
